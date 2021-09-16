f8e175840ba9045420fdf11c72a7069de1ca5d17
CPAanalytics <79522568+CPAanalytics@users.noreply.github.com>
2021-09-16 15:57:54 -0500

    First Commit - Banking File Parser

---
+++ EODPARSER_WF/EODForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace EODPARSER_WF
+{
+    partial class EodForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Parse = new System.Windows.Forms.Button();
+            this.GetFile = new System.Windows.Forms.Button();
+            this.FilenameBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Parse
+            // 
+            this.Parse.Location = new System.Drawing.Point(620, 372);
+            this.Parse.Name = "Parse";
+            this.Parse.Size = new System.Drawing.Size(143, 51);
+            this.Parse.TabIndex = 0;
+            this.Parse.Text = "Parse";
+            this.Parse.UseVisualStyleBackColor = true;
+            this.Parse.Click += new System.EventHandler(this.Parse_Click);
+            // 
+            // GetFile
+            // 
+            this.GetFile.Location = new System.Drawing.Point(72, 43);
+            this.GetFile.Name = "GetFile";
+            this.GetFile.Size = new System.Drawing.Size(143, 25);
+            this.GetFile.TabIndex = 1;
+            this.GetFile.Text = "Open EOD File";
+            this.GetFile.UseVisualStyleBackColor = true;
+            this.GetFile.Click += new System.EventHandler(this.GetFile_Click);
+            // 
+            // FilenameBox
+            // 
+            this.FilenameBox.Location = new System.Drawing.Point(274, 41);
+            this.FilenameBox.Name = "FilenameBox";
+            this.FilenameBox.Size = new System.Drawing.Size(346, 20);
+            this.FilenameBox.TabIndex = 2;
+            // 
+            // EODForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FilenameBox);
+            this.Controls.Add(this.GetFile);
+            this.Controls.Add(this.Parse);
+            this.Name = "EODForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox FilenameBox;
+        private System.Windows.Forms.Button Parse;
+        private System.Windows.Forms.Button GetFile;
+
+        #endregion
+    }
+}
---
+++ EODPARSER_WF/EODForm.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NSlice;
+
+namespace EODPARSER_WF
+{
+    public partial class EodForm : Form
+    {
+        private string _filePath;
+        private OpenFileDialog _fileDialog;
+
+        public EodForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GetFile_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = @"txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    _filePath = openFileDialog.FileName;
+                    _fileDialog = openFileDialog;
+                }
+            }
+
+            FilenameBox.Text = _filePath;
+        }
+
+        private void Parse_Click(object sender, EventArgs e)
+        {
+            using var file = new StreamReader(_fileDialog.FileName);
+            using StreamWriter writefile = new("export.csv", true);
+            var csvHeader =
+                "TransmitDate,ClearDate,CompanyNumber,CompanyName,Debit,Credit";
+            writefile.WriteLine(csvHeader);
+            {
+                string line;
+                decimal runningTotal = 0;
+                var transmitDate = string.Empty;
+                var clearDate = string.Empty;
+                while ((line = file.ReadLine()) != null)
+                {
+                    var prefix = line.Slice(0, 2);
+                    if (prefix == "90") break;
+
+                    if (prefix == "52")
+                    {
+                        transmitDate = "20" + line.Slice(63, 69);
+                        clearDate = "20" + line.Slice(69, 75);
+                    }
+
+                    if (prefix == "62")
+                    {
+                        var prAmount = line.Slice(29, 39);
+                        var decprAmount = decimal.Parse(prAmount) / 100;
+                        var coNumber = line.Slice(39, 44);
+                        var compName = line.Slice(54, 75);
+                        var csvLine =
+                            $"{transmitDate},{clearDate},{coNumber},{compName},{decprAmount}";
+
+
+                        writefile.WriteLine(csvLine);
+                        runningTotal += decprAmount;
+                    }
+
+                    if (line.Slice(0, 2) == "82")
+                    {
+                        var totalAmount = line.Slice(20, 32);
+                        var dectotalAmount = decimal.Parse(totalAmount) / 100;
+                        if (runningTotal != dectotalAmount) MessageBox.Show(@"Warning Total Does not foot to sum!");
+                        var csvLine = $"Total,,,,,{dectotalAmount}";
+                        writefile.WriteLine(csvLine);
+                    }
+                }
+
+                MessageBox.Show(@"Parse Complete.");
+            }
+        }
+    }
+}
